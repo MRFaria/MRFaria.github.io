8b22908937025b7b65d7334a9cc5a7f8c9676def
M R Faria <m.r.d.faria@gmail.com>
2017-06-14 14:50:04 +0100

    improved level

--- main.js
+++ main.js
@@ -5,21 +5,27 @@ var mainState = {
 
     preload: function() {
         game.load.image('player', 'assets/player.png');
+        game.load.image('wallH', 'assets/wallHorizontal.png');
+        game.load.image('wallV', 'assets/wallVertical.png');
     },
 
     create: function() {
         game.stage.backgroundColor = '#3498db';
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.renderer.renderSession.roundPixels = true;
+
         this.player = game.add.sprite(game.width / 2, game.height / 2, 'player');
         this.player.anchor.setTo(0.5, 0.5);
         game.physics.arcade.enable(this.player);
         this.player.body.gravity.y = 500;
+        
         this.cursor = game.input.keyboard.createCursorKeys();
+        this.createWorld();
     },
 
 
-    update: function() {
+    update: function () {
+        game.physics.arcade.collide(this.player, this.walls);
         this.movePlayer();
     },
 
@@ -50,8 +56,33 @@ var mainState = {
         }
     },
 
-};
+    createWorld: function () {
+        this.walls = game.add.group();
+        this.walls.enableBody = true;
 
+        game.add.sprite(0, 0, 'wallV', 0, this.walls); // Left wall
+        game.add.sprite(480, 0, 'wallV', 0, this.walls); // Right wall
+        game.add.sprite(0, 0, 'wallH', this.walls);      // Top left
+        game.add.sprite(300, 0, 'wallH', 0, this.walls); // Top right
+        game.add.sprite(0, 320, 'wallH', 0, this.walls); // Bottom left
+        game.add.sprite(300, 320, 'wallH', 0, this.walls); // Bottom right
+
+        game.add.sprite(-100, 160, 'wallH', 0, this.walls); // Middle left
+        game.add.sprite(400, 160, 'wallH', 0, this.walls); // Middle right
+
+        var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+        middleTop.scale.setTo(1.5, 1);
+        var middleBottom = game.add.sprite(100, 240, 'wallH', 0,
+            this.walls);
+        middleBottom.scale.setTo(1.5, 1);
+
+        this.walls.setAll('body.immovable', true);
+
+        
+
+
+    },
+};
 
 
 var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
